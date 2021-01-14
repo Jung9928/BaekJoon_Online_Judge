df97057243dae5541a2ff1922b2f5b67ab241ccd
Jung9928 <45419456+Jung9928@users.noreply.github.com>
2021-01-14 19:02:06 +0900

    Add files via upload

---
+++ C++/2750번(수 정렬하기).cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	vector<int> data;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		scanf("%d", &temp);
+		data.push_back(temp);
+	}
+
+	// sort() 함수를 사용하여 오름차순으로 정렬.
+	sort(data.begin(), data.end());
+	for (auto temp : data) {
+		cout << temp << '\n';
+	}
+
+	return 0;
+}
